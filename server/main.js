2f144268826190fbe4acd20a8b819627703dc24a
Noppadon Sodram <knopsod@gmail.com>
2018-05-22 07:09:24 +0700

    User see only data themself

--- server/main.js
+++ server/main.js
@@ -4,6 +4,6 @@ import { Teams } from '../imports/collections/teams';
 
 Meteor.startup(() => {
   Meteor.publish('teams', function () {
-    return Teams.find({ hiddenAt: null });
+    return Teams.find({ creatorId: this.userId, hiddenAt: null });
   });
 });
