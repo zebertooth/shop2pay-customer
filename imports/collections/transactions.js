c138e4b8488184596b1053e885ced4d54a3eac4b
Noppadon Sodram <knopsod@gmail.com>
2018-06-02 05:35:59 +0700

    Add WebApp API module

--- imports/collections/transactions.js
+++ imports/collections/transactions.js
@@ -1,8 +1,64 @@
 import { Mongo } from 'meteor/mongo';
 import { moment } from 'meteor/momentjs:moment';
 import { HTTP } from 'meteor/http';
+import { WebApp } from 'meteor/webapp';
 
 export const Transactions = new Mongo.Collection('transactions');
+
+if (Meteor.isServer) {
+  // Ref. https://forums.meteor.com/t/meteor-webapp-vs-picker-vs-simple-rest-for-rest-api/34034
+  // Ref. https://hashnode.com/post/web-api-using-meteor-webapp-ciqgn0ukj0irtdd53uy12h6ia
+  WebApp.connectHandlers.use('/api/transactions', (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+
+    if(req.method === 'POST') {
+
+      const {
+          client_url,
+          client_rest_api_endpoint,
+          client_transaction_id,
+          bank_account,
+          bank_no,
+          bank_name,
+          bank_short_name,
+          amount,
+          transferred_datetime,
+          is_approved,
+          approved_datetime
+      } = req.query;
+
+      const _id = Transactions.update(client_transaction_id,
+        {
+          $set:{
+            client_url,
+            client_rest_api_endpoint,
+            client_transaction_id,
+            bank_account,
+            bank_no,
+            bank_name,
+            bank_short_name,
+            amount,
+            transferred_datetime,
+            is_approved,
+            approved_datetime
+          }
+        }
+      );
+
+      if(_id) {
+        res.writeHead(201); // 201 Created
+        res.end();
+      } else {
+        res.writeHead(503); // 503 Service Unavailable
+        res.end();
+      }
+
+    } else {
+      res.writeHead(403); // 403 Forbiden
+      res.end();
+    }
+  });
+}
 
 Meteor.methods({
   'transactions.insert': function(transaction) {
@@ -19,13 +75,15 @@ Meteor.methods({
        // https://themeteorchef.com/tutorials/using-the-http-package
        // https://www.tutorialspoint.com/meteor/meteor_http.htm
        const endpoint = 'http://shop2pay-dev-test.herokuapp.com/api/transactions';
-       HTTP.call('POST', endpoint, {
+       HTTP.post(endpoint, {
          data: {
            ...one,
            client_transaction_id: _id,
            transferred_datetime: one.createdAt
          }
        }, (error, response) => {
+         console.log(error);
+         console.log(response);
          if(error) {
            Transactions.remove(_id);
          }
