85865376d592769d6904da0c2143558d170d15ce
Noppadon Sodram <knopsod@gmail.com>
2018-06-01 10:52:56 +0700

    Commit and push heroku for live test

--- imports/collections/transactions.js
+++ imports/collections/transactions.js
@@ -1,14 +1,38 @@
 import { Mongo } from 'meteor/mongo';
 import { moment } from 'meteor/momentjs:moment';
+import { HTTP } from 'meteor/http';
 
 export const Transactions = new Mongo.Collection('transactions');
 
 Meteor.methods({
   'transactions.insert': function(transaction) {
-     return Transactions.insert({
+     const _id = Transactions.insert({
        ...transaction,
        createdAt: moment().valueOf(),
        creatorId: this.userId
      });
+     const one = Transactions.findOne(_id);
+     console.log(one);
+     // return _id;
+     try {
+       // https://docs.meteor.com/api/http.html
+       // https://themeteorchef.com/tutorials/using-the-http-package
+       // https://www.tutorialspoint.com/meteor/meteor_http.htm
+       const endpoint = 'http://shop2pay-dev-test.herokuapp.com/api/transactions';
+       HTTP.call('POST', endpoint, {
+         data: {
+           ...one,
+           client_transaction_id: _id,
+           transferred_datetime: one.createdAt
+         }
+       }, (error, response) => {
+         if(error) {
+           Transactions.remove(_id);
+         }
+       });
+       return _id;
+     } catch (e) {
+       return false;
+     }
   }
 });
