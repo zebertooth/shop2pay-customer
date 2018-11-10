8fb3de1e5c348ebd2183f8d5dd2635856e98080c
Noppadon Sodram <knopsod@gmail.com>
2018-11-11 06:06:21 +0700

    set rest api endpoint to 27.254.163.57

--- imports/collections/transactions.js
+++ imports/collections/transactions.js
@@ -59,10 +59,10 @@ Meteor.methods({
         // Ref. https://docs.meteor.com/api/http.html
         // Ref. https://themeteorchef.com/tutorials/using-the-http-package
         // Ref. https://www.tutorialspoint.com/meteor/meteor_http.htm
-        const endpoint = 'http://localhost:3000/api/v1/transactions';
+        // const endpoint = 'http://localhost:3000/api/v1/transactions';
         // const endpoint = 'http://shop2pay-dev-test.herokuapp.com/api/v1/transactions';
         // const endpoint = 'http://www.shop2paytest.tk/api/v1/transactions';
-        // const endpoint = 'http://27.254.163.57/api/v1/transactions';
+        const endpoint = 'http://27.254.163.57/api/v1/transactions';
        HTTP.call('POST', endpoint, {
          data: {
            ...one,
